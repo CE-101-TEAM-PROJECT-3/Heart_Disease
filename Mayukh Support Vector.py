96188fcca02e58b8e01d0f033c4c1aa4a6c0da19
mayukh0506 <mayukhproject@gmail.com>
2026-01-30 02:47:59 +0000

    Fix variable name and import typo in SVC script

--- Mayukh Support Vector.py
+++ Mayukh Support Vector.py
@@ -4,14 +4,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score , classification report , confusion_matrix
+from sklearn.metrics import accuracy_score , classification_report , confusion_matrix
 # Load Dataset
 data = pd.read_csv("heart.csv")
 X = data.drop("target" , axis=1)
 y = data["target"]
 
 # Split the data set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_State=42,stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42, stratify=y)
 # Scale Features
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
@@ -23,6 +23,6 @@ svc.fit(X_train_scaled, y_train)
 y_pred_svc = svc.predict(X_test_scaled)
 
 print("SVC - Answer")
-print("Validation:",accuracy_score(y_test, y_pred))
+print("Validation:",accuracy_score(y_test, y_pred_svc))
 print("Classification report:\n", classification_report(y_test, y_pred_svc))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred_svc))
